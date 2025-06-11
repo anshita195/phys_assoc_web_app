29ae49c9397b8238c88e9c663fc821488fb15363
anshita195 <anshitajain195@gmail.com>
2025-06-11 18:58:54 +0530

    changes to server.js

--- server/server.js
+++ server/server.js
@@ -14,15 +14,6 @@ app.use('/api/research', require('./routes/research'));
 app.use('/api/events', require('./routes/events'));
 app.use('/api/courses', require('./routes/courses'));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
